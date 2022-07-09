fa753342785f67cbcd5a04716395fddafc623728
limi-chory <limi-chory>
2022-07-09 13:59:57 +0900

    Feat Add Styles and select function

--- src/components/Map/type.ts
+++ src/components/Map/type.ts
@@ -1,19 +1,7 @@
-export type AreaColorType = {
-  Seoul: string;
-  Busan: string;
-  Daegu: string;
-  Incheon: string;
-  Gwangju: string;
-  Daejeon: string;
-  Ulsan: string;
-  Gyeonggi: string;
-  Gangwon: string;
-  NorthChungcheong: string;
-  SouthChungcheong: string;
-  NorthJeolla: string;
-  SouthJeolla: string;
-  NorthGyeongsang: string;
-  SouthGyeongsang: string;
-  Jeju: string;
-  Sejong: string;
+export type AreaVisitedType = {
+  [area: string]: boolean;
+};
+
+export type AreaPathDataType = {
+  [area: string]: string;
 };
