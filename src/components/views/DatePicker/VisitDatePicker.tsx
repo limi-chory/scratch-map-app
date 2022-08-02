b587c39bc5189a039c530f14a0b7c97812e321de
limi-chory <limi-chory>
2022-08-02 22:17:36 +0900

    Fix: Remove unusable props and divide datepicker input button

--- src/components/views/DatePicker/VisitDatePicker.tsx
+++ src/components/views/DatePicker/VisitDatePicker.tsx
@@ -4,39 +4,44 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const VisitDatePicker: React.FC = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
 
-    const onChange = (dates: [Date, Date]) => {
-        const [start, end] = dates;
-        setStartDate(start);
-        setEndDate(end);
-    };
+  const onChange = (dates: [Date, Date]) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
 
-    // TODO seo : 아래와 같이 JSX 컴포넌트를 따로 정의해서 DatePicker 에 넘겨주는것 어떻게 하는지 모르겠음
-    interface Foo {
-        value: any;
-        onClick: any;
-    }
-    const CustomDatePickerInput = forwardRef<HTMLButtonElement, Foo>((prop: { value: any, onClick: any }, ref: any) => (
-        <button className="example-custom-input" onClick={prop.onClick} ref={ref}>
-            {prop.value}
-        </button>
-    ));
+  // TODO seo : 아래와 같이 JSX 컴포넌트를 따로 정의해서 DatePicker 에 넘겨주는것 어떻게 하는지 모르겠음
+
+  const CustomDatePickerInput = forwardRef<HTMLButtonElement, any>((prop: any, ref: any) => {
+    const dates = prop.value.split(" - ");
 
     return (
-        <div className="container">
-            <DatePicker
-                selected={startDate}
-                onChange={onChange}
-                startDate={startDate}
-                endDate={endDate}
-                dateFormat="yyyy년 MM월 dd일"
-                customInput={<CustomDatePickerInput value={undefined} onClick={undefined} />}
-                selectsRange
-            />
-        </div>
+      <>
+        <button className="example-custom-input" onClick={prop.onClick} ref={ref}>
+          {dates[0] ? dates[0] : "Start Date"}
+        </button>
+        <button className="example-custom-input" onClick={prop.onClick} ref={ref}>
+          {dates[1] ? dates[1] : "End Date"}
+        </button>
+      </>
     );
+  });
+
+  return (
+    <div className="container">
+      <DatePicker
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        dateFormat="yyyy년 MM월 dd일"
+        customInput={<CustomDatePickerInput />}
+        selectsRange
+      />
+    </div>
+  );
 };
 
 export default VisitDatePicker;
